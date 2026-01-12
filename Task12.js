11bbf9d933df324c95ee6d08e01b090e47fcb05e
NotGhoost <nikostr322@gmail.com>
2026-01-12 10:50:39 +0300

    Update Task12.js

--- Task12.js
+++ Task12.js
@@ -1,22 +1,8 @@
-function nthLongestString(arr, n) {
-  
-  if (n < 1 || n > arr.length) {
-    return null;
-  }
-
-   let withIndex = [];
-  for (let i = 0; i < arr.length; i++) {
-    withIndex.push({ str: arr[i], index: i });
-  }
-
-  withIndex.sort(function(a, b) {
-    if (a.str.length === b.str.length) {
-      return a.index - b.index;     }
-    return b.str.length - a.str.length;   });
-
-  return withIndex[n - 1].str;
+function longest(arr, n) {
+  return arr
+    .map((str, index) => ({ str, index }))
+    .sort((a, b) => {
+      const lenDiff = b.str.length - a.str.length;
+      return lenDiff !== 0 ? lenDiff : a.index - b.index;
+    })[n - 1].str;
 }
-let words = ["apple", "banana", "pear", "watermelon", "kiwi"];
-console.log(nthLongestString(words, 1)); 
-console.log(nthLongestString(words, 2)); 
-console.log(nthLongestString(words, 3)); 
